9b00d073beddca6df74065876f9662275e68f32c
xaime <xaime.rey@udc.es>
2023-11-08 17:29:38 +0100

    p6 parcialmente

--- p6/mylist.ml
+++ p6/mylist.ml
@@ -23,4 +23,59 @@ let compare_lengths l1 l2 =
   in aux l1 l2;;
 
 let compare_length_with l1 n =
-  let rec aux laux
+  let rec aux laux i =
+    if laux = [] && i = 0 then
+      0
+    else if laux = [] then
+      (-1)
+    else if i = 0 then
+      1
+    else
+      aux (tl laux) (i-1)
+  in aux l1 n;;
+
+let init len f =
+  let rec aux i l =
+    if i = 0 then
+      l
+    else
+      aux (i-1) ((f i)::l)
+  in aux len [];;
+
+let nth l n =
+  let rec aux (h::t) i =
+    if i = n then
+      h
+    else
+      aux t (i+1)
+  in aux l 0;;
+
+let rec append l1 l2 =
+  match l1 with
+  [] -> l2
+  | h::t -> h :: append t l2;;
+
+let rec rev_append l1 l2 = (* preguntar si terminal *)
+  match l1 with
+  [] -> l2
+  | h::t -> rev_append t (h::l2);; 
+
+let rev l =
+  let rec aux laux reversed =
+    match laux with
+    [] -> reversed
+    | h::t -> aux t (h::reversed)
+  in aux l [];;
+
+let concat l =
+  match l with
+  [] -> []
+  | h::t -> let rec aux l1 cola =
+              match cola with
+              [] -> l1
+              | z::y -> aux (append l1 z) y
+            in aux h t;;
+
+let flatten 
+
+let split
